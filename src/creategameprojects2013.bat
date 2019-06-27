67fe18e4ae754ff21e485ae40eee2960d633c6dc
unknown <ethbot@hotmail.com>
2019-06-27 22:33:47 +0100

    Clean-up Some VPC Files.

--- src/creategameprojects2013.bat
+++ src/creategameprojects2013.bat
@@ -1,5 +1,13 @@
-devtools\bin\vpc.exe /deferred +game /mksln Game_Deferred-2013.sln /2013
-devtools\bin\vpc.exe /template +game /mksln Game_Template-2013.sln /2013
-devtools\bin\vpc.exe /portal_template +game /mksln Game_Portal_Template-2013.sln /2013
-devtools\bin\vpc.exe /hl2_template +game /mksln Game_HL2_Template-2013.sln /2013
-devtools\bin\vpc.exe /vectronic +game /mksln Game_Vectronic-2013.sln /2013
+@echo off
+rem devtools\bin\vpc.exe /deferred +game /mksln Game_Deferred-2013.sln /2013
+rem devtools\bin\vpc.exe /template +game /mksln Game_Template-2013.sln /2013
+rem devtools\bin\vpc.exe /portal_template +game /mksln Game_Portal_Template-2013.sln /2013
+rem devtools\bin\vpc.exe /hl2_template +game /mksln Game_HL2_Template-2013.sln /2013
+rem devtools\bin\vpc.exe /vectronic +game /mksln Game_Vectronic-2013.sln /2013
+
+rem Keeing old entries for reasons.
+
+devtools\bin\vpc.exe /roriuslife +game /mksln Game_Rorius_Life.sln /2013
+echo Completed Making Solution at
+echo %TIME%
+pause
